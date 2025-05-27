6ab8da45158d58741f02e977db9fd57f6a39f2f4
Hao Liu <hao@simular.ai>
2025-05-26 22:44:15 -0700

    add arg for eval pipeline

--- pysimular/browser.py
+++ pysimular/browser.py
@@ -15,7 +15,9 @@ class SimularBrowser:
                  planner_mode: str = 's0',
                  allow_parallel_browsing: bool = False,
                  max_parallelism: int = 5,
-                 max_steps: int = 200):
+                 max_steps: int = 200,
+                 allow_replan: bool = True,
+                 test_env: str = 'production'):
         """Browser interface for Simular app.
 
         Args:
@@ -24,6 +26,8 @@ class SimularBrowser:
             allow_parallel_browsing (bool, optional): Whether to allow parallel browsing.
             max_parallelism (int, optional): Maximum number of parallel browser tabs.
             max_steps (int, optional): Maximum number of agent steps.
+            allow_replan (bool, optional): Whether to allow replanning.
+            test_env (str, optional): test environment to use, options: 'production', 'staging', 'local'.
         """
         self.app_path = path
         self.completion_event = threading.Event()
@@ -36,6 +40,8 @@ class SimularBrowser:
         self.info = {}
         self.tabs = {}
         self._setup_notification_observers()
+        self.allow_replan = allow_replan
+        self.test_env = test_env
 
     def _setup_notification_observers(self):
         """Setup observers for app responses and completion signal."""
@@ -116,7 +122,9 @@ class SimularBrowser:
             "allow_parallel_browsing": self.allow_parallel_browsing,
             "max_parallelism": self.max_parallelism,
             "max_steps": self.max_steps,
-            "reset": reset
+            "reset": reset,
+            "allow_replan": self.allow_replan,
+            "test_env": self.test_env
         }
         notification_name = self.bundle_id
         # print(f"Sending message with notification name: {notification_name}")
